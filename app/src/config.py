8cc58a997513219279b6d7c289a3c3633fed1d2b
yra3 <https://github.com/yra3>
2023-06-12 23:33:00 +0500

    fix alembic

--- app/src/config.py
+++ app/src/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings, PostgresDsn
 
 
 class Config(BaseSettings):
-    DATABASE_URL: PostgresDsn = "postgresql://postgres:postgres@localhost:5432/"
+    DATABASE_URL: PostgresDsn = "postgresql://postgres:postgres@postgres:5432/"
     SITE_DOMAIN: str = "localhost"
     DEBUG: bool = True
     APP_VERSION: str = "1"
